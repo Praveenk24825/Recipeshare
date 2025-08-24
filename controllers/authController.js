4a1400fec727a9453091f6dfd753a18b21135d9b
Praveenk24825 <praveenk24825@gmail.com>
2025-08-24 19:39:38 +0530

    dvm

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+// authController.js
 import asyncHandler from "express-async-handler";
 import User from "../models/User.js";
 
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,11 @@ import User from "../models/User.js";
 
 export const protect = asyncHandler(async (req, res, next) => {
   let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -14,7 +18,9 @@ export const protect = asyncHandler(async (req, res, next) => {
       res.status(401);
       throw new Error("Not authorized, token failed");
     }
-  } else {
+  }
+
+  if (!token) {
     res.status(401);
     throw new Error("Not authorized, no token");
   }
