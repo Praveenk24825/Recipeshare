f813f781dc31d9ed6a12071e20fcccd08022e164
Praveenk24825 <praveenk24825@gmail.com>
2025-08-26 12:19:17 +0530

    save changes

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  profileImage: { type: String, default: "" }, 
   followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 }, { timestamps: true });
